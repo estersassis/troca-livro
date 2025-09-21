2b53fc85e375e1fbbbb435ca5a3ae1265085f1f4
Ester Sara Assis Silva <ester.assis@prosperita.io>
2025-09-21 20:48:08 -0300

    feat: added base views

--- trocalivro/library/views.py
+++ trocalivro/library/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def display_book_image(book):
+    ...
+
+def index(request):
+    ...
+
+
+def signup(request):
+    ...
+
+@login_required
+def profile(request):
+    ...
+
+@login_required
+def edit_profile(request):
+    ...
+
+@login_required
+def book_add(request):
+    ...
+
+@login_required
+def my_books(request):
+    ...
+
+@login_required
+def send_books(request):
+    ...
+
+@login_required
+def received_books(request):
+    ...
+
+def book_detail_view(request, id):
+    ...
+
+def search_book(request):
+    ...
+    
+# Função responsável por tratar as trocas entre os livros 
+def request(request, id):
+    ...
